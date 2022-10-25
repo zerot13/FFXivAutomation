d37dc0e4ac42e8cd46c61abddbede6595b51eaba
Ian Machuca <imac0.13@gmail.com>
2022-10-25 09:50:22 -0700

    Add Crafting Automation
    #4

---
+++ FFXivAutomation/Crafting/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace FFXivAutomation.Crafting
+{
+    public enum CraftType
+    {
+        GiantPopoto,
+        PerillaOil,
+        ScallopSalad,
+        Gelatin,
+        PalmSugar,
+        RSykonBav,
+        CommandingDraught,
+        GoldenHoney,
+        UplandWheatFlour
+    }
+
+    public enum CraftKeys
+    {
+        LeftClick,
+        Key_1
+    }
+}
---
+++ FFXivAutomation/Crafting/HqMaterial.cs
@@ -0,0 +1,14 @@
+﻿namespace FFXivAutomation.Crafting
+{
+    public class HqMaterial
+    {
+        public HqMaterial(int row, int count)
+        {
+            Row = row;
+            Count = count;
+        }
+
+        public int Row { get; }
+        public int Count { get; }
+    }
+}
